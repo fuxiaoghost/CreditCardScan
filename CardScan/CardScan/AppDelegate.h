3f5376edf068e662fcf09d2f542415c5b3f3fb81
DawnWang <wangsguang@gmail.com>
2013-11-26 13:49:23 +0800

    scan

---
+++ CardScan/CardScan/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  CardScan
+//
+//  Created by Dawn on 13-11-26.
+//  Copyright (c) 2013年 Dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>{
+@private
+    UIImageView *cardView;
+}
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
